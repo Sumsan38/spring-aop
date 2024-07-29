f868edce137d46bff21f574778ff1b3a62532e37
Sumsan38 <ndanl4647@gmail.com>
2024-07-29 23:02:16 +0900

    within 1

---
+++ src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,36 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WithinTest {
+    /**
+     * within 관련 <br/>
+     * within 지시자는 특정 타입 내의 조인 포인트들로 매칭을 제한한다. 쉽게 이야기해서 해당 타입이 매칭되면 그 안의 메서드(조인 포인트)들이 자동으로 매칭된다.<br/>
+     * 문법은 단순한데 execution 에서 타입 부분만 사용한다고 보면 된다.<br/>
+     */
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact(){
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+
+
+}
