b6847a46cba09089e4853cbe055f4b685f2d050d
ZoySNam <zoy.s.nam@selvas.com>
2024-07-30 11:29:08 +0900

    within 2 (완료)

--- src/test/java/hello/aop/pointcut/WithinTest.java
+++ src/test/java/hello/aop/pointcut/WithinTest.java
@@ -3,6 +3,7 @@ package hello.aop.pointcut;
 import hello.aop.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
@@ -31,6 +32,36 @@ public class WithinTest {
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
+    @Test
+    void withinStart(){
+        pointcut.setExpression("within(hello.aop.member.*Service*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
 
+    @Test
+    void withinSubPackage(){
+        pointcut.setExpression("within(hello.aop..*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
 
+    /**
+     * within 사용시 주의해야 할 점이 있다. <br/>
+     * 표현식에 부모 타입을 지정하면 안된다는 점이다.<br/>
+     * 정확하게 타입이 맞아야 한다. <br/>
+     * 이 부분에서 execution 과 차이가 난다.<br/>
+     */
+
+    @Test
+    @DisplayName("타켓의 타입에만 직접 적용, 인터페이스를 선정하면 안된다")
+    void withinSuperTypeFalse(){
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스를 선정 가능")
+    void executionSuperTypeTrue(){
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
 }
