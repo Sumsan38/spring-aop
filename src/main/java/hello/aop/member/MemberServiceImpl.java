9d7c1a5613eb95eb4c2138d433839c3e787d5c4d
Sumsan38 <ndanl4647@gmail.com>
2024-08-08 22:25:31 +0900

    @annotation, @args

--- src/main/java/hello/aop/member/MemberServiceImpl.java
+++ src/main/java/hello/aop/member/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.aop.member;
 
 import hello.aop.member.annotation.ClassAop;
+import hello.aop.member.annotation.MethodAop;
 import org.springframework.stereotype.Component;
 
 @ClassAop
@@ -9,6 +10,7 @@ public class MemberServiceImpl implements MemberService {
     // AOP 포인트컷 테스트를 위한 소스
 
     @Override
+    @MethodAop("test value")
     public String hello(String param) {
         return "ok";
     }
